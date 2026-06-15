d11827202fa606c6e916f2f4d971b40e57b96fb8
SaibaK01 <it24006@mbstu.ac.bd>
2026-06-15 16:48:21 +0600

    Update ShapeMain.java

--- shapes/ShapeMain.java
+++ shapes/ShapeMain.java
@@ -1,4 +1,5 @@
 package shapes;
+
 import java.util.Scanner;
 
 public class ShapeMain {
@@ -7,63 +8,55 @@ public class ShapeMain {
 
         Scanner sc = new Scanner(System.in);
 
-        // TASK 1
         int n = sc.nextInt();
-
-        // TASK 2
         Shape[] shapes = new Shape[n];
 
-        // TASK 3
         for (int i = 0; i < n; i++) {
 
             String type = sc.next();
             String color = sc.next();
-            boolean filled = sc.nextBoolean();
+            String f = sc.next();
+            boolean filled = f.equalsIgnoreCase("true");
 
-            if (type.equals("RECTANGLE")) {
+            if (type.equalsIgnoreCase("RECTANGLE")) {
 
-                double width = sc.nextDouble();
-                double length = sc.nextDouble();
+                double w = sc.nextDouble();
+                double l = sc.nextDouble();
 
-                shapes[i] = new Rectangle(color, filled, width, length);
+                shapes[i] = new Rectangle(color, filled, w, l);
 
-            } else if (type.equals("CIRCLE")) {
+            } else {
 
-                double radius = sc.nextDouble();
+                double r = sc.nextDouble();
 
-                shapes[i] = new Circle(color, filled, radius);
+                shapes[i] = new Circle(color, filled, r);
             }
         }
 
-        // TASK 4
-        for (Shape s : shapes) {
-            System.out.println(s);
-            System.out.println();
+        for (int i = 0; i < n; i++) {
+            System.out.println(shapes[i]);
+            if (i < n - 1) System.out.println();
         }
 
-        // TASK 5
+        System.out.println();
         System.out.println("--- Downcast Check ---");
 
         for (Shape s : shapes) {
-
             if (s instanceof Rectangle) {
-
                 Rectangle r = (Rectangle) s;
-
-                System.out.println("Rectangle width=" + r.getWidth()
-                        + " length=" + r.getLength());
-
-            } else if (s instanceof Circle) {
-
+                System.out.println("Rectangle width=" + r.getWidth() + " length=" + r.getLength());
+            } else {
                 Circle c = (Circle) s;
-
                 System.out.println("Circle radius=" + c.getRadius());
             }
         }
+        //Task -6:
+        //Answer: The compiler will give Compile time error if I write: Shape s = new Shape("Red", true); 
+        // Since Shape is an abstract class therefore it can't be instantiated.
 
-   // ── TASK 6:
-          //Answer: the compiler give compile-time error if I write: Shape s = new Shape("Red", true);
-          //  Since Shape is an abstract class therefore it can't be instantiated.
         sc.close();
     }
 }
+
+
+
